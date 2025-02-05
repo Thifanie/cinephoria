ea5e1a4d2065ed30b74072028f8a26457953a3fe
Thifanie <thifanie640@gmail.com>
2025-02-05 16:12:18 +0100

    Maj vues pages principales

--- cinephoria-workspace/projects/cinephoria-web/src/app/app.component.ts
+++ cinephoria-workspace/projects/cinephoria-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './features/menus/nav/nav.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './features/menus/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, RouterOutlet],
+  imports: [NavComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- cinephoria-workspace/projects/cinephoria-web/src/app/pages/films/films.component.ts
+++ cinephoria-workspace/projects/cinephoria-web/src/app/pages/films/films.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ListFilmsComponent } from '../../features/films/components/list-films/list-films.component';
 
 @Component({
   selector: 'app-films',
-  imports: [],
+  imports: [ListFilmsComponent],
   templateUrl: './films.component.html',
-  styleUrl: './films.component.css'
+  styleUrl: './films.component.css',
 })
-export class FilmsComponent {
-
-}
+export class FilmsComponent {}
