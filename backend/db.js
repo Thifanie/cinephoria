f93d3c61535bda13d4eda0c6db00d7f7e1750c23
Thifanie <thifanie640@gmail.com>
2025-03-18 16:23:06 +0100

    Maj config BDD pour déploiement Railway

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,13 @@
-const mariadb = require("mariadb");
+const mysql = require("mysql2");
+
 require("dotenv").config();
 
-const pool = mariadb.createPool({
-  host: process.env.DB_HOST || "host.docker.internal",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   connectionLimit: 10,
   acquireTimeout: 10000, // Timeout d'acquisition plus long
   waitForConnections: true, // Attend une connexion libre au lieu de planter
