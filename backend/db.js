e6da9b3fc615e5399932d002b06455cb4fadac70
Thifanie <thifanie640@gmail.com>
2025-03-19 12:27:33 +0100

    Erreur hébergement : maj db.js

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,13 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   connectionLimit: 10,
   waitForConnections: true, // Attend une connexion libre au lieu de planter
   connectTimeout: 10000, // Timeout de connexion
