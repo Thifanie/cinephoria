bd513e5c8e1071ce686cb91ca8726ed781e48a73
Thifanie <thifanie640@gmail.com>
2025-03-19 13:03:38 +0100

    Erreur déploiement railway : maj dataservice et création .env frontend

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -19,8 +19,7 @@ export class DataService {
   public films$ = this.filmsSubject.asObservable();
   constructor(private readonly http: HttpClient) {}
   private readonly apiUrl =
-    (window as any)['API_URL'] ||
-    'https://mon-backend-production.up.railway.app'; // 'http://localhost:3000/api';
+    (window as any)['API_URL'] || process.env['API_URL']; // 'http://localhost:3000/api';
 
   filmData: Film[] = [];
 
