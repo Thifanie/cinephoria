84961384e42dc07e7972d9450323182760af0e93
Thifanie <thifanie640@gmail.com>
2025-04-02 18:26:13 +0200

    Correction secrets

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || "shuttle.proxy.rlwy.net",
-  user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "WQFTfSUkYnwNqqaiMdJiHegLcCcCQqLj",
-  database: process.env.MYSQLDATABASE || "railway",
-  port: process.env.MYSQLPORT || "11449",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   connectionLimit: 10,
   waitForConnections: true, // Attend une connexion libre au lieu de planter
   connectTimeout: 10000, // Timeout de connexion
